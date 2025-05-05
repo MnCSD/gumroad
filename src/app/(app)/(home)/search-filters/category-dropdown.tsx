7621bd49a1ac6807e3250769310e2c3e6b3e74a5
MnCSD <mnikolopoylos@gmail.com>
2025-05-05 16:32:17 +0300

    categories-finalization

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -53,10 +53,9 @@ export const CategoryDropdown = ({
               "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]"
           )}
         >
-          <Link
-            href={`/${category.slug === "all" ? "" : category.slug}`}
-          ></Link>
-          {category.name}
+          <Link href={`/${category.slug === "all" ? "" : category.slug}`}>
+            {category.name}
+          </Link>
         </Button>
         {category.subcategories && category.subcategories.length > 0 && (
           <div
